20540ff4db61ec1c255d03f9e84fccdaadbd3b34
DominikSzkotland <dszlencka@outlook.com>
2024-07-15 14:13:24 +0200

    altitude and left spwn bugs fixed

--- scripts/spawnSubmarine.js
+++ scripts/spawnSubmarine.js
@@ -3,19 +3,25 @@ const spawnSubmarine = () => {
     submarine.classList.add('submarine');
     const leftOrRight = Math.random() < 0.5 ? 0 : 1;
     document.getElementById('ocean').appendChild(submarine);
-    submarine.style.left = leftOrRight === 0 ? 'calc(' + leftOrRight * 100 + '% - ' + submarine.offsetWidth + 'px)' : leftOrRight * 100 + '%';
+    if(leftOrRight === 0) {
+        submarine.style.left = 'auto';
+        submarine.style.right = '0';}
+    else{
+        submarine.style.right = 'auto';
+        submarine.style.left = '0';}
     submarine.style.top = Math.random() * 100 + '%';
     adjustAltitude(submarine);
     return submarine;
 }
 
 const adjustAltitude = (submarine) => {
+    
     const oceanHeight = document.getElementById('ocean').offsetHeight;
     const submarineAltitude = submarine.offsetTop;
     const submarineHeight = submarine.offsetHeight;
-    if(oceanHeight + submarineAltitude < oceanHeight + submarineHeight) {
-        submarine.top = 'auto';
-        submarine.bottom = '0';
+    if(oceanHeight - submarineAltitude < submarineHeight) {console.log(submarine)
+        submarine.style.top = 'auto';
+        submarine.style.bottom = '0';
     }
 }
 
