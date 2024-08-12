856ebac8a5df95cd516eb4d75ccda475914da519
p3flox <komandotoja@gmail.com>
2024-08-12 22:48:59 +0200

    changed px to skallable percent

--- scripts/spawnUnderShip.js
+++ scripts/spawnUnderShip.js
@@ -3,11 +3,15 @@ const spawnUnderShip = (TemplateOfElementToSpawn = {'elementType':undefined, 'st
     const elementToSpawn = document.createElement('div');
     const shipElement = document.getElementById('ship');
     document.getElementById('ocean').appendChild(elementToSpawn);
+    const oceanElement = document.getElementById('ocean');
     elementToSpawn.classList.add(TemplateOfElementToSpawn.styleClass);
     elementToSpawn.setAttribute('data-elementType', TemplateOfElementToSpawn.elementType);
     sizeAdjuster(elementToSpawn);
     elementToSpawn.style.top = 0;
-    elementToSpawn.style.left = `${shipElement.offsetLeft + (shipElement.offsetWidth / 2)}px`;
+    const shipLeftRelativeToOcean = shipElement.offsetLeft - oceanElement.offsetLeft;
+    const leftPercent = ((shipLeftRelativeToOcean + (shipElement.offsetWidth / 2)) / oceanElement.offsetWidth) * 100;
+    elementToSpawn.style.left = `${leftPercent}%`;
+
     return elementToSpawn;
 }
 
